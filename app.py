eecd76d1ca8a7f93975b5bdc503b74b25ecab1b2
Matheus0118 <hmatheus8@hotmail.com>
2025-06-19 09:34:56 -0300

    Atualizando arquivos

--- app.py
+++ app.py
@@ -31,5 +31,5 @@ app.register_blueprint(chamado_bp)
 
 
 if __name__ == '__main__':
-        port = int(os.environ.get("PORT", 5432))
+        port = int(os.environ.get("PORT", 5000))
         app.run(debug=False, host='0.0.0.0', port=port)
