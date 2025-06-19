7cb21411a5027d881275fb75c617aa41b8165a5d
Matheus0118 <hmatheus8@hotmail.com>
2025-06-19 09:15:52 -0300

    Atualizando arquivos

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_cors import CORS
 from database import init_app, db
@@ -30,4 +31,5 @@ app.register_blueprint(chamado_bp)
 
 
 if __name__ == '__main__':
-        app.run(debug=True, host='0.0.0.0', port=5000)
+        port = int(os.environ.get("PORT", 5432))
+        app.run(debug=False, host='0.0.0.0', port=port)
--- database.py
+++ database.py
@@ -4,6 +4,6 @@ from flask import Flask
 db = SQLAlchemy()
 
 def init_app(app: Flask):
-    app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:Mathadm@localhost:5432/OTEPSolucoes_db'
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:EpyrEGAwhyUIkhboQrEpwGiBpNdVRKSc@postgres.railway.internal:5432/railway'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
